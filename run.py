2f608c4eebdb2b81ee9010cdcc032c3f956c2945
Volodymyr <djevl333@gmail.com>
2025-05-19 14:03:11 +0300

    Fix relative import. Change requirements.txt

--- run.py
+++ run.py
@@ -10,13 +10,8 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 if current_dir not in sys.path:
     sys.path.append(current_dir)
 
-# import and launch GUI
-if os.path.basename(current_dir) == 'fetch_url':
-    # when running from inside the fetch_url directory
-    from gui import tk, ImageProcessorApp
-else:
-    # when running from parent directory
-    from .gui import tk, ImageProcessorApp
+
+from gui import tk, ImageProcessorApp
 
 if __name__ == "__main__":
     root = tk.Tk()
